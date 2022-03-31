2d1f7c33a3ce0369b85f7195da68996cf395759c
Bestine <bestineokinda@gmail.com>
2022-03-31 15:50:01 +0300

    added a python file

---
+++ testPython.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+numbers = np.linspace(0, 100, 2)
+sns.set()
+plt.plot(numbers)
+print("Hello Mustafa")
+plt.show()
